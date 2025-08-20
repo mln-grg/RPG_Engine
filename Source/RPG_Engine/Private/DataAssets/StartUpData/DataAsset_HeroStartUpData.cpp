6be699c3a9bce44841c2d0eab61b643e451e2a6c
mln-grge <milankgeorge@gmail.com>
2025-08-21 00:41:29 +0530

    Added player startup abilities

--- Source/RPG_Engine/Private/DataAssets/StartUpData/DataAsset_HeroStartUpData.cpp
+++ Source/RPG_Engine/Private/DataAssets/StartUpData/DataAsset_HeroStartUpData.cpp
@@ -3,3 +3,26 @@
 
 #include "DataAssets/StartUpData/DataAsset_HeroStartUpData.h"
 
+#include "AbilitySystem/Abilities/RPGGameplayAbility.h"
+
+bool FRPGHeroAbilitySet::IsValid() const
+{
+	return InputTag.IsValid() && AbilityToGrant;
+}
+
+void UDataAsset_HeroStartUpData::GiveToAbilitySystemComponent(URPGAbilitySystemComponent* InASCToGive, int32 ApplyLevel)
+{
+	Super::GiveToAbilitySystemComponent(InASCToGive, ApplyLevel);
+
+	for (const FRPGHeroAbilitySet& AbilitySet : HeroStartUpAbilitySets)
+	{
+		if(!AbilitySet.IsValid()) continue;
+
+		FGameplayAbilitySpec AbilitySpec(AbilitySet.AbilityToGrant);
+		AbilitySpec.SourceObject = InASCToGive->GetAvatarActor();
+		AbilitySpec.Level = ApplyLevel;
+		AbilitySpec.DynamicAbilityTags.AddTag(AbilitySet.InputTag);
+
+		InASCToGive->GiveAbility(AbilitySpec);
+	}
+}
--- Source/RPG_Engine/Public/DataAssets/StartUpData/DataAsset_HeroStartUpData.h
+++ Source/RPG_Engine/Public/DataAssets/StartUpData/DataAsset_HeroStartUpData.h
@@ -3,8 +3,23 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
 #include "DataAssets/StartUpData/DataAsset_StartUpDataBase.h"
 #include "DataAsset_HeroStartUpData.generated.h"
+
+USTRUCT(BlueprintType)
+struct FRPGHeroAbilitySet
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,meta = (Categories = "InputTag"))
+	FGameplayTag InputTag;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TSubclassOf<URPGGameplayAbility> AbilityToGrant;
+
+	bool IsValid() const;
+};
 
 /**
  * 
@@ -13,5 +28,11 @@ UCLASS()
 class RPG_ENGINE_API UDataAsset_HeroStartUpData : public UDataAsset_StartUpDataBase
 {
 	GENERATED_BODY()
+
+public:
+	virtual void GiveToAbilitySystemComponent(URPGAbilitySystemComponent* InASCToGive, int32 ApplyLevel) override;
+private:
+	UPROPERTY(EditDefaultsOnly, Category="StartUpData",meta = (TitleProperty = "InputTag"))
+	TArray<FRPGHeroAbilitySet> HeroStartUpAbilitySets;
 	
 };
