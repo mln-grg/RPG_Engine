c0e6e66dd9d9c40823867a7bd3ea49c449d1a7fc
mln-grge <milankgeorge@gmail.com>
2025-08-24 14:26:42 +0530

    Some renaming and cleanup to keep things generic

--- Source/RPG_Engine/Private/RPGGameplayTags.cpp
+++ Source/RPG_Engine/Private/RPGGameplayTags.cpp
@@ -8,16 +8,21 @@ namespace RPGGameplayTags
 	/** Input Tags **/
 	UE_DEFINE_GAMEPLAY_TAG(InputTag_Move,"InputTag.Move");
 	UE_DEFINE_GAMEPLAY_TAG(InputTag_Look,"InputTag.Look");
-	UE_DEFINE_GAMEPLAY_TAG(InputTag_EquipAxe,"InputTag.EquipAxe");
-	UE_DEFINE_GAMEPLAY_TAG(InputTag_UnequipAxe,"InputTag.UnequipAxe");
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_EquipWeapon,"InputTag.EquipWeapon");
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_UnequipWeapon,"InputTag.UnequipWeapon");
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_LightAttack,"InputTag.LightAttack");
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_HeavyAttack,"InputTag.HeavyAttack");
 	
 
 	/** Player Tags **/
-	UE_DEFINE_GAMEPLAY_TAG(Player_Ability_Equip_Axe,"Player.Ability.Equip.Axe");
-	UE_DEFINE_GAMEPLAY_TAG(Player_Ability_Unequip_Axe,"Player.Ability.Unequip.Axe");
+	UE_DEFINE_GAMEPLAY_TAG(Player_Ability_Equip_Weapon,"Player.Ability.Equip.Weapon");
+	UE_DEFINE_GAMEPLAY_TAG(Player_Ability_Unequip_Weapon,"Player.Ability.Unequip.Weapon");
+	
+	UE_DEFINE_GAMEPLAY_TAG(Player_Ability_Attack_Light,"Player.Ability.Attack.Light");
+	UE_DEFINE_GAMEPLAY_TAG(Player_Ability_Attack_Heavy,"Player.Ability.Attack.Heavy");
 	
 	UE_DEFINE_GAMEPLAY_TAG(Player_Weapon_Axe,"Player.Weapon.Axe");
 	
-	UE_DEFINE_GAMEPLAY_TAG(PlayerEvent_Equip_Axe,"Player.Event.Equip.Axe");
-	UE_DEFINE_GAMEPLAY_TAG(PlayerEvent_Unequip_Axe,"Player.Event.Unequip.Axe");
+	UE_DEFINE_GAMEPLAY_TAG(PlayerEvent_Equip_Weapon,"Player.Event.Equip.Weapon");
+	UE_DEFINE_GAMEPLAY_TAG(PlayerEvent_Unequip_Weapon,"Player.Event.Unequip.Weapon");
 }
